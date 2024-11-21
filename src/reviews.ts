58df21c728ec76434ec2e62ae89d7dd9f014c0d5
Irvin Tancioco <irvin.tanc@gmail.com>
2024-11-21 00:08:48 -0800

    fix reviews.ts

--- src/reviews.ts
+++ src/reviews.ts
@@ -120,13 +120,22 @@ export const getGitFile = async (
         },
       }
     );
-    //@ts-ignore
-    const decodedContent = Buffer.from(
-      response.data.content,
-      "base64"
-    ).toString("utf8");
-    //@ts-ignore
-    return { content: decodedContent, sha: response.data.sha };
+
+    if (
+      !Array.isArray(response.data) && // Ensure it's not a directory listing
+      response.data.type === "file" && // Ensure it's a file
+      "content" in response.data // Check if `content` exists
+    ) {
+      //@ts-ignore
+      const decodedContent = Buffer.from(
+        response.data.content,
+        "base64"
+      ).toString("utf8");
+      //@ts-ignore
+      return { content: decodedContent, sha: response.data.sha };
+    } else {
+      return { content: null, sha: null };
+    }
   } catch (exc) {
     if (exc.status === 404) {
       return { content: null, sha: null };
